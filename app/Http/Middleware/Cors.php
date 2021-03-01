c714ef4ebc4a16c01a70a1e160e9ac35ea5dcfdd
orlando-rojas <rojasmoncada.orlando@gmail.com>
2021-02-28 21:25:48 -0500

    fix cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,12 +8,11 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
+
         return $next($request)
-            //Url a la que se le dará acceso en las peticiones
-            ->header("Access-Control-Allow-Origin", "http://localhost:3000")
-            //Métodos que a los que se da acceso
-            ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-            //Headers de la petición
-            ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', ' Origin, Content-Type, Accept, Authorization, X-Request-With')
+            ->header('Access-Control-Allow-Credentials', ' true');
     }
 }
